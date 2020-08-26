bb3d7b59f2da9c39d3a30ca5960953b3f285384b
Sofia <sofia.lau.alves@gmail.com>
2020-08-25 23:14:45 -0400

    added fixed nav and some media queries

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 // ** JS CODE ** // 
 
-$(function () {
-  
+$(function () {  
+  App.init ();
 })
+
+const App = {};
+
+App.init = function() {
+  App.eventListener();
+}
+
+// NAV // 
+// when user is at the welcome page (header) the Nav should be white in colour
+// colour changes when each i leaves the header and is completely in the main section. It will remain the same colour in the main section. 
+// The icon will change depending on the section the user is on. Either it will change in colour or will change to the title of the section. 
+// When the user hovers a icon, it will show the title of the section, which will replace the icon. 
+
+// Projects //
+// The projects will fade in and appear as the user scrolls to that section , leading with the image, and text following behind. 
+
+
+App.eventListener = function () {
+
+}
